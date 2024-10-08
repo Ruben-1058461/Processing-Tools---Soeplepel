1f66dcd81d1c532c9ad28a3a09c509759d804310
Ruben <ruben@gateway.nl>
2024-10-08 11:27:11 +0200

    Add fixture

--- api/tests/test_auth_provider_integration.py
+++ api/tests/test_auth_provider_integration.py
@@ -1,2 +1,8 @@
 import pytest
 from providers.auth_provider import init, get_user, has_access
+
+# Door fixture is auth provider altijd geinitialiseerd
+@pytest.fixture(autouse=True)
+def setup_auth_provider():
+    """Initialiseer authprovider met gebruikergegevens."""
+    init()
