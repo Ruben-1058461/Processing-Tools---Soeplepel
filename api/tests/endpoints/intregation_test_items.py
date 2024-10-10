0143bf5f6c53286f5ad17becf59960745878b858
Thijs <1051036@hr.nl>
2024-10-10 19:55:14 +0200

    Added: Test DELETE for items

--- api/tests/endpoints/intregation_test_items.py
+++ api/tests/endpoints/intregation_test_items.py
@@ -1,7 +1,7 @@
 import unittest
 import requests
 
-class TestItemGroupAPI(unittest.TestCase):
+class TestItemsAPI(unittest.TestCase):
     def setUp(self):
         # Base URL for your backend API
         self.base_url = f'http://localhost:3000/api/v1/items'
@@ -15,10 +15,10 @@ class TestItemGroupAPI(unittest.TestCase):
     
     def test_get_item_item_by_id(self):
         # Define the item item ID to retrieve
-        item_item_id = 'P000001' # input("Choose an ID ")
+        item_id = 'P000001' # input("Choose an ID ")
         
         # Send the GET request to retrieve the item item by ID
-        response = requests.get(f'{self.base_url}/{item_item_id}', headers=self.headers)
+        response = requests.get(f'{self.base_url}/{item_id}', headers=self.headers)
 
         # Assert the response status code (200 for successful retrieval)
         self.assertEqual(response.status_code, 200)
@@ -50,10 +50,10 @@ class TestItemGroupAPI(unittest.TestCase):
     }
 
         # Define the item item ID to update
-        item_item_id = 'P011721'  # input("Choose an ID ")
+        item_id = 'P011721'  # input("Choose an ID ")
 
         # Send the PUT request to retrieve the item item by ID
-        response = requests.post(f'{self.base_url}/{item_item_id}', headers=self.headers, json=data)
+        response = requests.post(f'{self.base_url}/{item_id}', headers=self.headers, json=data)
 
         # Assert the response status code
         self.assertEqual(response.status_code, 200)
@@ -84,17 +84,28 @@ class TestItemGroupAPI(unittest.TestCase):
     }
 
         # Define the item item ID to update
-        item_item_id = 'P011721'  # input("Choose an ID ")
+        item_id = 'P011721'  # input("Choose an ID ")
 
         # Send the PUT request to retrieve the item item by ID
-        response = requests.put(f'{self.base_url}/{item_item_id}', headers=self.headers, json=data)
+        response = requests.put(f'{self.base_url}/{item_id}', headers=self.headers, json=data)
 
         # Assert the response status code
         self.assertEqual(response.status_code, 200)
         if response.status_code == 201:
             print(f"The requested ID has been succesfully updated. {data}")
 
+    def test_delete_items(self):
 
+        # Define the item ID to update
+        item_id = 'P011721'  # input("Choose an ID ")
+
+        # Send the PUT request to retrieve the item item by ID
+        response = requests.delete(f'{self.base_url}/{item_id}', headers=self.headers)
+
+        # Assert the response status code (200 or 201 depending on your API)
+        self.assertEqual(response.status_code, 200)
+        if response.status_code == 200:
+            print("The requested ID has been succesfully deleted.")
 
 
         
