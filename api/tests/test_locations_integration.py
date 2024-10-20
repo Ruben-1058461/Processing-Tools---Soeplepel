6c54368b18390af4c0b75315267188202b50d524
Ruben-1058461 <1058461@hr.nl>
2024-10-20 14:06:53 +0200

    Add 3rd integration

--- api/tests/test_locations_integration.py
+++ api/tests/test_locations_integration.py
@@ -31,9 +31,37 @@ def test_add_and_get_location_integration(locations):
     assert result["name"] == "Location 1"
 
 
-def test_not_existing_location(locations):
+def test_not_existing_location_integration(locations):
     """Integratietest voor het ophalen van niet bestaande locatie."""
     # Haal locatie op die niet bestaat
     result = locations.get_location(123456) # Fake id
 
     assert result is None # Dit moet uitkomen op None
+
+def test_updating_location_integration(locations):
+    """Integratietest voor het bijwerken van location"""
+    location = {
+        "id": 1,
+        "warehouse_id": 101,
+        "name": "Location 1"
+    }
+
+    # Voeg locatie toe
+    locations.add_location(location)
+
+    updated_location = {
+        "id": 1,
+        "warehouse_id": 102,
+        "name": "Updated location 1"
+    }
+
+    
+    locations.update_location(1, updated_location)
+
+    # Haal de locatie opnieuw op
+    result = locations.get_location(1)
+
+    
+    assert result is not None  # De locatie moet bestaan
+    assert result["warehouse_id"] == 102  # De naam moet zijn bijgewerkt
+    assert result["name"] == "Updated location 1"  # De naam moet zijn bijgewerkt
