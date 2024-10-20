40575ff19ae605eaec03de2cc016cdc73a95e82b
Ruben-1058461 <1058461@hr.nl>
2024-10-20 12:44:14 +0200

    Add unit test testing adding location.

--- api/tests/test_locations.py
+++ api/tests/test_locations.py
@@ -1,1 +1,21 @@
 import pytest
+from models.locations import Locations
+
+@pytest.fixture
+def locations():
+    """Init locations met debug on"""
+    return Locations(root_path='', is_debug=True)
+
+def test_add_location(locations):
+    """Test toevoegen locatie"""
+    location = {
+        "id": 1, # Unieke id
+        "warehouse_id": 102, # ID van magazijn
+        "name": "Location 1" # Naam locatie
+    }
+    locations.add_location(location) # Voeg locatie toe aan in memory
+
+    # 1 Controlleer of eht aantal locaties in data nu 1 is
+    assert len(locations.data) == 1
+    # 2 Controlleer of de toegevoegde locatie de juiste naam heeft
+    assert locations.data[0]["name"] == "Location 1"
